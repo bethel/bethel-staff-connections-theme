88118d764c4f448162c86d2011f8c545c2c66d2a
Chris Roemmich <chris@cr-wd.com>
2013-08-05 17:08:14 -0500

    initial commit

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+if (!class_exists('BethelStaffCard')) {
+    class BethelStaffCard
+    {
+        public function __construct()
+        {
+
+        }
+    }
+
+    $this->BethelStaffCard = new BethelStaffCard();
+}
